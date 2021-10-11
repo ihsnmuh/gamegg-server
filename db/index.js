9f9c1e8767608e44a8ed0440988412e4b2011930
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-11 16:57:44 +0700

    convert adminLTE to category page

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { urlDb } = require('../config');
+
+mongoose.connect(urlDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: true,
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
